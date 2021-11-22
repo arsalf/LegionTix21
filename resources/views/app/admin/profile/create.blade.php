56620402c5effae7184f37a0d9d0dd491c41b276
Arsal Fadilah <arsal.fadilah.tif420@polban.ac.id>
2021-11-22 09:21:39 +0700

    update web
    testtt

--- resources/views/app/admin/profile/create.blade.php
+++ resources/views/app/admin/profile/create.blade.php
@@ -5,6 +5,11 @@
 @endsection
 
 @section('custom_css')
+<script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+<script src="http://api.iksgroup.co.id/apijs/lokasiapi.js"></script>
+<script>
+var render=createwidgetlokasi("provinsi","kabupaten","kecamatan","kelurahan");
+</script>
 <style>
 .form-control:focus {
     box-shadow: none;
@@ -64,21 +69,71 @@
                         <h4 class="text-right">Profile Settings</h4>
                     </div>
                     <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                        <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="surname"></div>
+                        <div class="col-md-6">
+                            <label class="labels">First Name</label>
+                            <input type="text" class="form-control" placeholder="First Name" value="">
+                        </div>
+                        <div class="col-md-6">
+                            <label class="labels">Last Name</label>
+                            <input type="text" class="form-control" value="" placeholder="Last Name">
+                        </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Phone Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                        <div class="col-md-12"><label class="labels">Address</label><input type="text" class="form-control" placeholder="enter address" value=""></div>
-                        <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
+                        <div class="col-md-12">
+                            <label class="labels">Phone Number</label>
+                            <input type="text" class="form-control" placeholder="Phone Number" value="">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
                         <div class="col-md-6">
                             <label class="labels">Date Birth</label>    
                             <input class="form-control" type="date" id="birthday" name="birthday">
                         </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-6"><label class="labels">City</label><input type="text" class="form-control" placeholder="country" value=""></div>
-                        <div class="col-md-6"><label class="labels">Province</label><input type="text" class="form-control" value="" placeholder="state"></div>
+                        <div class="col-md-4">
+                            <label class="labels">Rt</label>
+                            <input type="text" class="form-control" placeholder="Rt" value="">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="labels">Rw</label>
+                            <input type="text" class="form-control" value="" placeholder="Rw">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="labels">Kelurahan</label>
+                            <input type="text" class="form-control" value="" placeholder="Kelurahan">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <label class="labels">Kecamatan</label>
+                            <input type="text" class="form-control" placeholder="Kecamatan" value="">
+                        </div>
+                        <div class="col-md-6">
+                            <label class="labels">Kode Pos</label>
+                            <input type="text" class="form-control" value="" placeholder="Kode Pos">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-6">
+                            <label class="labels">City</label>
+                            <input type="text" class="form-control" placeholder="City" value="">
+                        </div>
+                        <div class="col-md-6">
+                            <label class="labels">Province</label>
+                            <select id="provinsi" class="form-control">
+                                <option></option>
+                            </select>
+                            <select id="kabupaten" class="form-control">
+                                <option></option>
+                            </select>
+                            <select id="kecamatan" class="form-control">
+                                <option></option>
+                            </select>
+                            <select id="kelurahan" class="form-control">
+                                <option></option>
+                            </select>
+                        </div>
                     </div>
                     <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
                 </div>
@@ -88,4 +143,17 @@
 </div>
 </div>
 
+@endsection
+
+
+@section('custom_js')
+
+
+<script>
+    $(document).ready(function(){
+                $("#show").click(function(){
+                    $("#output").html(trackdatalokasi);
+                });            
+            });   
+</script>
 @endsection
